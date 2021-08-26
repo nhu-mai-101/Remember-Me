36262612d03e4fc1b957b76dff87034dfddebc0b
Nhu Mai <nhumaix3@gmail.com>
2021-08-26 03:22:16 -0700

    full functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import styled from 'styled-components';
-import Card from './Card.jsx';
+import Cards from './Card.jsx';
+import background from '../../../images/background.jpg';
+import WinnerModal from './Modal.jsx';
 
 const Board = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   flex-wrap: wrap;
-  width: 750px;
+  ${({ difficulty }) => {
+    if (difficulty === 'easy') return `
+      width: 450px;
+    `
+    if (difficulty === 'medium') return `
+      width: 600px;
+    `
+    if (difficulty === 'hard') return `
+      width: 750px;
+    `
+  }};
 `;
 
-const StartOver = styled.button`
+const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -18,30 +30,146 @@ const StartOver = styled.button`
   width: 150px;
   border: 3px solid black;
   border-radius: 10px;
+  margin: 10px;
+  cursor: pointer;
 `;
 
 const Page = styled.div`
   display: flex;
   justify-content: center;
+  // background-image: url(${background});
 `;
 
-const Title = styled.div`
-  display:flex;
-  justify-content:center;
-`;
+const shuffleCards = (array) => {
+  var n = array.length;
+  while (n) {
+    var random = Math.floor(Math.random() * n--);
+    var temp = array[n];
+    array[n] = array[random];
+    array[random] = temp;
+  }
+  return array;
+}
 
 const App = ({ planets }) => {
-  const [cards, setCards] = useState(planets.concat(planets));
+  const easy = planets.slice(0, 3);
+  const medium = planets.slice(0, 6);
+  const hard = planets;
+
+  const [difficulty, setDifficulty] = useState('medium');
+  const [cards, setCards] = useState(shuffleCards(medium.concat(medium)));
+  const [openedCards, setOpenedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+  const timeout = useRef(null);
+
+  useEffect(() => {
+    let timeout = null;
+    if (openedCards.length === 2) {
+      timeout = setTimeout(checkForMatch, 500);
+    }
+    return () => {
+      clearTimeout(timeout)
+    };
+  }, [openedCards]);
+
+  useEffect(() => {
+    checkCompletion();
+  }, [matchedCards]);
+
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
+
+  const checkCompletion = () => {
+    if (matchedCards.length === (cards.length / 2)) {
+      setShowModal(true);
+    }
+  };
+
+  const handleCardClick = (index) => {
+    if (openedCards.length === 1) {
+      setOpenedCards(openedCards.concat([index]))
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenedCards([index]);
+    }
+  };
+
+  const checkIsFlipped = (index) => {
+    return openedCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return matchedCards.includes(card.name);
+  };
+
+  const checkForMatch = () => {
+    const first = openedCards[0];
+    const second = openedCards[1];
+    enable();
+    if (cards[first].name === cards[second].name) {
+      setMatchedCards(matchedCards.concat([cards[first].name]));
+      console.log(matchedCards)
+      setOpenedCards([]);
+      return;
+    }
+    timeout.current = setTimeout(() => {
+      setOpenedCards([]);
+    }, 600);
+  };
+
+  const handleStartOver = () => {
+    setDifficulty('medium');
+    setMatchedCards([]);
+    setOpenedCards([]);
+    setShowModal(false);
+    setCards(shuffleCards(medium.concat(medium)));
+  };
+
+  const handleEasy = () => {
+    setDifficulty('easy');
+    setMatchedCards([]);
+    setOpenedCards([]);
+    setShowModal(false);
+    setCards(shuffleCards(easy.concat(easy)));
+  };
+
+  const handleHard = () => {
+    setDifficulty('hard');
+    setMatchedCards([]);
+    setOpenedCards([]);
+    setShowModal(false);
+    setCards(shuffleCards(hard.concat(hard)));
+  };
+
   return (
     <Page>
-        <Title>Remember Me</Title>
-      <Board>
+      <WinnerModal show={showModal} handleStartOver={handleStartOver} />
+      <Board difficulty={difficulty}>
         {cards.map((card, index) => {
           return (
-            <Card card={card} key={index} />
+            <Cards
+              card={card}
+              key={index}
+              index={index}
+              handleCardClick={handleCardClick}
+              isDisabled={shouldDisableAllCards}
+              isInactive={checkIsInactive(card)}
+              isFlipped={checkIsFlipped(index)}
+            />
           )
         })}
-        <StartOver>START OVER</StartOver>
+        <Button onClick={handleEasy} >EASY</Button>
+        <Button onClick={handleStartOver}>MEDIUM</Button>
+        <Button onClick={handleHard}>HARD</Button>
+        <Button onClick={handleStartOver}>START OVER</Button>
       </Board>
     </Page>
   )
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import back from '../../../images/back.png'
+import ReactCardFlip from 'react-card-flip';
 
-const Photo = styled.img`
+const Front = styled.img`
   border-radius: 20px;
   margin: auto;
   padding: 5px;
 `;
 
-const Card = ({ card }) => {
+const Back = styled.img`
+  border-radius: 20px;
+  margin: auto;
+  padding: 5px;
+  cursor: pointer;
+  ${({ isInactive }) => isInactive && `
+    opacity: 0;
+  `}
+`;
+
+const Card = ({ card, index, handleCardClick, isDisabled, isInactive, isFlipped }) => {
+  const handleClick = () => {
+    !isFlipped && !isDisabled && handleCardClick(index);
+  };
+
  return (
-   <div>
-     <Photo src={card.image.default} />
+   <div className="scene">
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div className="back">
+        <Back src={back} onClick={isInactive ? () => {} : handleClick} isInactive={isInactive} />
+      </div>
+      <div className="front">
+        <Front src={card.image.default}/>
+      </div>
+    </ReactCardFlip>
    </div>
  )
 }
